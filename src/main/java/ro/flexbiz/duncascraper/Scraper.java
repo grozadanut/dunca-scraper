af3b1b2a4c4ce599e18a22956f0c4be57161c948
Groza Danut <groza_danut@yahoo.com>
2026-01-26 09:24:27 +0200

    Wrote dunca tile info scraper

--- src/main/java/ro/flexbiz/duncascraper/Scraper.java
+++ src/main/java/ro/flexbiz/duncascraper/Scraper.java
@@ -1,20 +1,71 @@
 package ro.flexbiz.duncascraper;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Locator.WaitForOptions;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 @Component
 public class Scraper {
-	public String findTileInfo() {
+	@Value("${dunca.url}") private String url;
+	@Value("${dunca.username}") private String username;
+	@Value("${dunca.password}") private String password;
+	
+	public List<Map<String, String>> findTileInfo(String tileName) {
 		try (Playwright playwright = Playwright.create()) {
             final Browser browser = playwright.chromium().launch();
             final Page page = browser.newPage();
-            page.navigate("https://playwright.dev/java/docs/intro");
-            System.out.println(page.title());
-            return page.title();
+            page.navigate(url);
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Username")).fill(username);
+            page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Password")).fill(password);
+            page.getByRole(AriaRole.BUTTON,
+            		new Page.GetByRoleOptions().setName(
+            				Pattern.compile("login", Pattern.CASE_INSENSITIVE)))
+            .click();
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Lista Stocuri")).click();
+            page.locator("#sotext-1084-inputEl").fill("*"+tileName+"*");
+            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Browse")).click();
+            
+            page.locator("#ext-gen1247").getByText("Loading...")
+            .waitFor(new WaitForOptions().setState(WaitForSelectorState.DETACHED));
+            
+            final boolean nothingFound = page.getByText("There is no data to display.").count() > 0;
+            
+            if (nothingFound)
+            	return List.of(Map.of("error", MessageFormat.format("Produsul {0} nu a fost gasit!", tileName))); //$NON-NLS-1$
+
+            final Locator rows = page.locator("#gridview-1127 table tbody tr");
+            final int rowCount = rows.count();
+            final List<Map<String, String>> result = new ArrayList<>();
+
+            for (int i = 0; i < rowCount; i++) {
+            	final Locator row = rows.nth(i);
+            	final Locator cells = row.locator("td");
+            	
+            	if (cells.nth(0).count() <= 0)
+            		continue;
+
+            	final String name = cells.nth(1).textContent();
+            	final String stockOradea = cells.nth(2).textContent();
+            	final String stockBucuresti = cells.nth(3).textContent();
+            	final String stockIasi = cells.nth(4).textContent();
+
+            	result.add(Map.of("name", name, "oradea", stockOradea, "bucuresti", stockBucuresti, "iasi", stockIasi));
+            }
+
+            return result;
         }
 	}
 }
--- src/main/java/ro/flexbiz/duncascraper/ScrapingController.java
+++ src/main/java/ro/flexbiz/duncascraper/ScrapingController.java
@@ -1,9 +1,13 @@
 package ro.flexbiz.duncascraper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +17,8 @@ public class ScrapingController {
 	
 	@Autowired private Scraper scraper;
 	
-	@GetMapping()
-    public ResponseEntity<String> scrape() {
-		return new ResponseEntity<>(scraper.findTileInfo(), HttpStatus.OK);
+	@GetMapping("/{tileName}")
+    public ResponseEntity<List<Map<String, String>>> scrape(@PathVariable final String tileName) {
+		return new ResponseEntity<>(scraper.findTileInfo(tileName), HttpStatus.OK);
     }
 }
