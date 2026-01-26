a8dce4b72af2de076ad66ef11991a07ebe093c60
Groza Danut <groza_danut@yahoo.com>
2026-01-26 09:34:28 +0200

    Push simple tests

--- src/test/java/ro/flexbiz/duncascraper/DuncaScraperApplicationTests.java
+++ src/test/java/ro/flexbiz/duncascraper/DuncaScraperApplicationTests.java
@@ -1,13 +1,21 @@
 package ro.flexbiz.duncascraper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DuncaScraperApplicationTests {
 
+	@Autowired private Scraper scraper;
+	
 	@Test
-	void contextLoads() {
+	void simpleCases() {
+		assertThat(scraper.findTileInfo("asd123asd1")).contains(Map.of("error", "Produsul asd123asd1 nu a fost gasit!"));
+		assertThat(scraper.findTileInfo("smoge aqua")).hasSizeGreaterThan(1);
 	}
-
 }
